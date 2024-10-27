dde12ed8199f00e361bf17b073ce5340a15cc1c5
Md Jubayer <mdjubayerislamshuvo34@gmail.com>
2024-10-27 23:13:45 +0600

    ok

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,6 @@ import { Provider } from 'react-redux';
 import { store } from './store/store';
 import axios from 'axios';
 
-// axios.defaults.baseURL = 'http://localhost:8080/api';
 axios.defaults.baseURL = 'https://portfolio-server-md-jubayer.vercel.app/api';
 axios.defaults.withCredentials = true;
 
